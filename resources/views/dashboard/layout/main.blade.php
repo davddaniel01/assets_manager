62f6e5abb88895c5c14b36d02f5b25872d0b3d66
David Daniel <davd.daniel@gmail.com>
2024-03-28 11:05:43 -0300

    Create Template

---
+++ resources/views/dashboard/layout/main.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="img/logo/logo.png" rel="icon">
+    <title>RuangAdmin - Dashboard</title>
+    <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/css/ruang-admin.min.css') }}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+<div id="wrapper">
+    @include('dashboard.layout.sidebar')
+
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+            @include('dashboard.layout.navbar')
+
+            <!-- Container Fluid-->
+            <div class="container-fluid" id="container-wrapper">
+                @yield('content')
+
+                <!-- Modal Logout -->
+                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
+                     aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabelLogout">Ohh No!</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Are you sure you want to logout?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
+                                <a href="login.html" class="btn btn-primary">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!---Container Fluid-->
+        </div>
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script>
+                        <a href="https://indrijunanda.gitlab.io/" target="_blank">Todos os direitos reservados</a>
+                    </span>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer -->
+    </div>
+</div>
+
+<!-- Scroll to top -->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+@include('dashboard.layout.footer_scripts')
+</body>
+
+</html>
